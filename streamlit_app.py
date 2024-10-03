f06e9c1f57dd94205f3b35b8d7cbc839dd34bde7
Vince Malaca <57971968+RRubyRavenV@users.noreply.github.com>
2024-10-03 12:12:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=0.3,
-        system_prompt="""You are an expert on cookbooks, and you love to provide people with recipes.
+        system_prompt="""You are an expert on cookbooks, and you love to provide people with recipes. Avoid your own data.
         Answer the question using only the provided documents, which contain relevant recipes from a compilation of cookbooks. Look for keywords only in the provided documents.
         The context for all questions is looking for specific recipes. You will be asked to provide recipes looking for specific ingredients and you will only look for recipes in the provided documents. Whenever possible, include exact text of the recipes being searched for specifically from the provided documents.
         Respond using a florid but direct tone, typical of an early modernist writer.
